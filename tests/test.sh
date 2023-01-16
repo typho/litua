28a3a3a1ef166c82b7285248eeb4deac53e3fa24
tajpulo <tajpulo@typho.org>
2023-01-16 05:10:14 +0100

    feature: use .lit file extension for source files, test file works relative to root

--- tests/test.sh
+++ tests/test.sh
@@ -3,20 +3,18 @@
 set -e
 
 TEMP_DIR="/tmp"
-PROJECT_DIR=".."
-TESTS_DIR="."
+PROJECT_DIR="."
+TESTS_DIR="tests"
 BIN="$PROJECT_DIR/target/debug/litua"
-
-# TODO needs to work from project directory, make directories in output short
 
 [ ! -f "$BIN" ] && echo "executable '$BIN' not found" && exit 1;
 
 for EXAMPLE in enumeration literate-programming markup replacements ; do
   mkdir -p "$TEMP_DIR/$EXAMPLE"
-  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.txt" -o "$TEMP_DIR/$EXAMPLE.out.actual"
-  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.txt" --dump-hooks > "$TEMP_DIR/$EXAMPLE.hooks.actual"
-  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.txt" --dump-lexed > "$TEMP_DIR/$EXAMPLE.lexed.actual"
-  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.txt" --dump-parsed > "$TEMP_DIR/$EXAMPLE.parsed.actual"
+  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" -o "$TEMP_DIR/$EXAMPLE.out.actual"
+  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" --dump-hooks > "$TEMP_DIR/$EXAMPLE.hooks.actual"
+  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" --dump-lexed > "$TEMP_DIR/$EXAMPLE.lexed.actual"
+  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" --dump-parsed > "$TEMP_DIR/$EXAMPLE.parsed.actual"
 
   diff -u "$TESTS_DIR/$EXAMPLE.out.expected" "$TEMP_DIR/$EXAMPLE.out.actual"
   diff -u "$TESTS_DIR/$EXAMPLE.hooks.expected" "$TEMP_DIR/$EXAMPLE.hooks.actual"
