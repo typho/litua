fdc9836407d0f7f5d079033f43a3e70e1b92c181
tajpulo <tajpulo@typho.org>
2023-01-24 02:50:12 +0100

    feature: cleanup of main.rs, fix stdout/stderr distinction, better CLI docu

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use std::str;
 use std::error;
 use std::fmt;
 
-// Error type
+// Error type (covers all error cases)
 #[derive(Debug)]
 enum Error {
     CLIArg(String),
@@ -65,18 +65,49 @@ impl From<mlua::Error> for Error {
     }
 }
 
+fn derive_destination_filepath(p: &path::Path) -> path::PathBuf {
+    if let Some(ext) = p.extension() {
+        if ext == "lit" {
+            p.with_extension("out")
+        } else {
+            p.with_extension("lit")
+        }
+    } else {
+        path::PathBuf::from("doc.out")
+    }
+}
 
-fn run_lua<A: AsRef<path::Path>, B: AsRef<path::Path>, C: AsRef<path::Path>>(_conf: &Settings, dst: A, doc: &tree::DocumentTree, hooks_dir: B, luapath_additions: C) -> Result<(), Error> {
+// auxiliary functions
+
+/// Determine the set of hook files in the directory at the given filepath
+fn find_hook_files(hooks_dir: &path::Path) -> Result<Vec<path::PathBuf>, io::Error> {
+    let mut hook_files = vec![];
+    for dir_entry in fs::read_dir(hooks_dir)? {
+        let entry = dir_entry?;
+        let basename = entry.file_name();
+        if let Some(name) = basename.to_str() {
+            if name.starts_with("hook") && name.ends_with(".lua") {
+                hook_files.push(entry.path());
+            }
+        }
+    }
+    Ok(hook_files)
+}
+
+/// Take a `conf` with settings and run the Lua runtime on the tree `doc`
+fn run_lua(conf: &Settings, doc: &tree::DocumentTree) -> Result<(), Error> {
     // NOTE: 'debug' library is only available with Lua::unsafe_new()
     //       https://github.com/khvzak/mlua/issues/39
     let lua = unsafe { Lua::unsafe_new() };
 
-    let addition_str = path::PathBuf::from(luapath_additions.as_ref());
-    match addition_str.to_str() {
-        Some(s) if !s.is_empty() => lua.load(&format!("package.path = package.path .. ';{s}'")).exec()?,
-        Some(_) => {},
-        None => return Err(Error::CLIArg("cannot convert the luapath extension path (supplied as --add-require-path) to a UTF-8 string. But this is sadly required by the mlua interface (the library to run Lua)".to_owned())),
-    };
+    for lua_path in conf.lua_path_additions.iter() {
+        let addition_str = path::PathBuf::from(&lua_path);
+        match addition_str.to_str() {
+            Some(s) if !s.is_empty() => lua.load(&format!("package.path = package.path .. ';{s}'")).exec()?,
+            Some(_) => {},
+            None => return Err(Error::CLIArg("cannot convert the luapath extension path (supplied as --add-require-path) to a UTF-8 string. But this is sadly required by the mlua interface (the library to run Lua)".to_owned())),
+        };
+    }
 
     // (1) load litua libraries
     let litua_table = include_str!("litua.lua");
@@ -85,20 +116,12 @@ fn run_lua<A: AsRef<path::Path>, B: AsRef<path::Path>, C: AsRef<path::Path>>(_co
     lua.load(litua_lib).set_name("litua.lib.lua")?.exec()?;
 
     // (2) find hook files
-    let mut hook_files = vec![];
-    for dir_entry in fs::read_dir(hooks_dir)? {
-        let entry = dir_entry?;
-        let basename = entry.file_name();
-        if let Some(name) = basename.to_str() {
-            if name.starts_with("hook") && name.ends_with(".lua") {
-                println!("Loading hook file '{name}'");
-                hook_files.push(entry.path());
-            }
-        }
-    }
+    let hook_files = find_hook_files(&conf.hooks_dir).map_err(Error::Io)?;
 
     // (3) read hook files
     for hook_file in hook_files.iter() {
+        eprintln!("Loading hook file '{}'", hook_file.display());
+
         let lua_file_src = fs::read_to_string(hook_file)?;
         let mut chunk = lua.load(&lua_file_src);
         {
@@ -121,56 +144,73 @@ fn run_lua<A: AsRef<path::Path>, B: AsRef<path::Path>, C: AsRef<path::Path>>(_co
     let globals = lua.globals();
     let global_litua: mlua::Table = globals.get("Litua")?;
     let transform: mlua::Function = global_litua.get("transform")?;
-    println!("Running transformation.");
+    eprintln!("Running transformation.");
     let lua_result = transform.call::<mlua::Value, mlua::String>(tree)?;
     let result = lua_result.to_str()?;
 
     // (7) print the result
-    fs::write(dst, result)?;
+    fs::write(&conf.destination, result)?;
 
     Ok(())
 }
 
-fn derive_destination_filepath(p: &path::Path) -> path::PathBuf {
-    if let Some(ext) = p.extension() {
-        if ext == "lit" {
-            p.with_extension("out")
-        } else {
-            p.with_extension("lit")
-        }
-    } else {
-        path::PathBuf::from("doc.out")
-    }
-}
-
-fn lex_and_parse(conf: &Settings, src: &path::Path) -> Result<tree::DocumentTree, Error> {
-    let mut fd = fs::File::open(src)?;
+/// Read the source file mentioned in `conf`, lex its source code,
+/// and then parse it. The resulting tree will be returned.
+fn lex_and_parse(conf: &Settings) -> Result<tree::DocumentTree, Error> {
+    let mut fd = fs::File::open(&conf.source)?;
     let mut buf = Vec::new();
     fd.read_to_end(&mut buf)?;
 
     let source_code = str::from_utf8(&buf)?;
     let l = lexer::Lexer::new(source_code);
 
-    if conf.dump_lexed {
-        for tok_or_err in l.iter() {
-            let token = tok_or_err?;
-            println!("Token= {token:?}");
-        }
-    } else if conf.dump_parsed {
-        let mut p = parser::Parser::new(src, source_code);
-        p.consume_iter(l.iter())?;
-        p.finalize()?;
+    let mut p = parser::Parser::new(&conf.source, source_code);
+    p.consume_iter(l.iter())?;
+    p.finalize()?;
 
-        println!("{:?}", p.tree());
-    } else {
-        let mut p = parser::Parser::new(src, source_code);
-        p.consume_iter(l.iter())?;
-        p.finalize()?;
+    Ok(p.tree())
+}
 
-        return Ok(p.tree());
+// Function invoked due to some operation argument
+
+/// Read the source file mentioned in `conf`, lex its source code,
+/// then parse it and finally process the resulting tree with Lua.
+/// In conclusion, this is Litua's default main routine.
+fn lex_and_parse_and_run(conf: &Settings) -> Result<(), Error> {
+    let doctree = lex_and_parse(conf)?;
+    println!("File '{}' read.", conf.source.display());
+
+    run_lua(conf, &doctree)?;
+    println!("File '{}' written.", conf.destination.display());
+
+    Ok(())
+}
+
+/// Read the source file mentioned in `conf`, lex its source code,
+/// then parse it and print the resulting tree. Useful for debugging.
+fn lex_and_parse_and_dump(conf: &Settings) -> Result<(), Error> {
+    let doctree = lex_and_parse(conf)?;
+    println!("{doctree:?}");
+
+    Ok(())
+}
+
+/// Read the source file mentioned in `conf` and lex its source code.
+/// Print the resulting sequence of tokens. Useful for debugging.
+fn lex_and_dump(conf: &Settings) -> Result<(), Error> {
+    let mut fd = fs::File::open(&conf.source)?;
+    let mut buf = Vec::new();
+    fd.read_to_end(&mut buf)?;
+
+    let source_code = str::from_utf8(&buf)?;
+    let l = lexer::Lexer::new(source_code);
+
+    for tok_or_err in l.iter() {
+        let token = tok_or_err?;
+        println!("Token= {token:?}");
     }
 
-    Ok(tree::DocumentTree::new())
+    Ok(())
 }
 
 #[derive(Parser, Debug)]
@@ -179,20 +219,20 @@ fn lex_and_parse(conf: &Settings, src: &path::Path) -> Result<tree::DocumentTree
 #[command(version = "0.9")]
 #[command(about = "Read document as tree and apply Lua functions to nodes")]
 #[command(author, version, about, long_about = None)]
-struct Settings {
+struct CLISettings {
     // helpful for debugging 
-    #[arg(long)]
+    #[arg(long, help = "if set, only prints the runtime configuration and exits")]
+    dump_config: bool,
+    #[arg(long, help = "if set, only lexes the source file, prints its tokens and exits")]
     dump_lexed: bool,
-    #[arg(long)]
+    #[arg(long, help = "if set, only parses the source file, prints the resulting tree and exits")]
     dump_parsed: bool,
-    #[arg(long)]
-    dump_hooks: bool,
 
     // configuration
-    #[arg(long, value_name = "DIR")]
+    #[arg(long, value_name = "DIR", help = "filepath to directory with hook files (default: same as source file)")]
     hooks_dir: Option<path::PathBuf>,
-    #[arg(long, value_name = "DIR")]
-    add_require_path: Option<path::PathBuf>,
+    #[arg(long, value_name = "DIR", help = "directories to add as search location for require(…) calls")]
+    add_require_path: Vec<path::PathBuf>,
 
     // optional argument
     #[arg(short = 'o', long, value_name = "PATH")]
@@ -202,35 +242,52 @@ struct Settings {
     source: path::PathBuf,
 }
 
+#[derive(Debug)]
+struct Settings {
+    hooks_dir: path::PathBuf,
+    lua_path_additions: Vec<path::PathBuf>,
+    source: path::PathBuf,
+    destination: path::PathBuf,
+}
 
 fn main() -> Result<(), Error> {
-    let conf = Settings::parse();
+    // CLI argument parsing
+    let settings = CLISettings::parse();
 
-    let src = conf.source.as_path();
-    let derived_dst = derive_destination_filepath(src);
-    let dst = match &conf.destination {
+    let derived_dst = derive_destination_filepath(&settings.source);
+    let dst = match &settings.destination {
         Some(p) => p.as_path(),
         None => derived_dst.as_path(),
     };
 
     let default_hooks_dir = path::PathBuf::from(".");
-    let default_lua_path_additions = path::PathBuf::from("");
-
-    let hooks_dir = match &conf.hooks_dir {
+    let hooks_dir = match &settings.hooks_dir {
         Some(d) => d.as_path(),
-        None => conf.source.parent().unwrap_or(default_hooks_dir.as_path()),
-    };
-    let lua_path_additions = match &conf.add_require_path {
-        Some(d) => d.as_path(),
-        None => &default_lua_path_additions,
+        None => settings.source.parent().unwrap_or(default_hooks_dir.as_path()),
     };
 
-    let doctree = lex_and_parse(&conf, src)?;
-    if !conf.dump_lexed && !conf.dump_parsed {
-        run_lua(&conf, dst, &doctree, hooks_dir, lua_path_additions)?;
-        println!("File '{}' read.", src.display());
-        println!("File '{}' written.", dst.display());
+    let mut lua_path_additions = vec![];
+    for dir in settings.add_require_path.iter() {
+        lua_path_additions.push(dir.to_owned());
     }
 
-    Ok(())
+    // define execution configuration
+    let conf = Settings {
+        hooks_dir: hooks_dir.to_owned(),
+        lua_path_additions,
+        source: settings.source,
+        destination: dst.to_owned(),
+    };
+
+    // run main routine
+    if settings.dump_config {
+        println!("{:?}", &conf);
+        Ok(())
+    } else if settings.dump_lexed {
+        lex_and_dump(&conf)
+    } else if settings.dump_parsed {
+        lex_and_parse_and_dump(&conf)
+    } else {
+        lex_and_parse_and_run(&conf)
+    }
 }
