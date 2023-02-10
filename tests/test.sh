a3210ad65076d0ad615559942419afe203ae6fe9
tajpulo <tajpulo@typho.org>
2023-02-10 12:23:42 +0100

    docs(version): Bump version number

--- tests/test.sh
+++ tests/test.sh
@@ -12,12 +12,10 @@ BIN="$PROJECT_DIR/target/release/litua"
 for EXAMPLE in enumeration literate-programming markup replacements ; do
   mkdir -p "$TEMP_DIR/$EXAMPLE"
   "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" -o "$TEMP_DIR/$EXAMPLE.out.actual"
-  "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" --dump-hooks > "$TEMP_DIR/$EXAMPLE.hooks.actual"
   "$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" --dump-lexed > "$TEMP_DIR/$EXAMPLE.lexed.actual"
   #"$BIN" "$PROJECT_DIR/examples/$EXAMPLE/doc.lit" --dump-parsed > "$TEMP_DIR/$EXAMPLE.parsed.actual"
 
   diff -u "$TESTS_DIR/$EXAMPLE.out.expected" "$TEMP_DIR/$EXAMPLE.out.actual"
-  diff -u "$TESTS_DIR/$EXAMPLE.hooks.expected" "$TEMP_DIR/$EXAMPLE.hooks.actual"
   diff -u "$TESTS_DIR/$EXAMPLE.lexed.expected" "$TEMP_DIR/$EXAMPLE.lexed.actual"
   #diff -u "$TESTS_DIR/$EXAMPLE.parsed.expected" "$TEMP_DIR/$EXAMPLE.parsed.actual"
 
