53b5456a62ed68b5c2952628c464c168a06b477b
tajpulo <tajpulo@typho.org>
2023-04-12 01:20:36 +0200

    docs: improve literate-programming example

--- examples/literate-programming/hooks.all.lua
+++ examples/literate-programming/hooks.all.lua
@@ -1,19 +1,23 @@
 local io = require("io")
 
 Litua.on_setup(function ()
-    Litua.global.file_code = io.open("src.code", "w")
-    Litua.global.file_docu = io.open("src.docu", "w")
+    Litua.global.file_code = io.open("code.txt", "w")
+    Litua.global.file_docu = io.open("docu.txt", "w")
 end)
 
 Litua.read_new_node("code", function (node)
     for i=1,#node.content do
-        Litua.global.file_code:write(tostring(node.content[i]))
+        Litua.global.file_code:write(node:totext())
+
+        Litua.global.file_docu:write("```\n")
+        Litua.global.file_docu:write(node:totext())
+        Litua.global.file_docu:write("\n```\n")
     end
 end)
 
 Litua.read_new_node("docu", function (node)
     for i=1,#node.content do
-        Litua.global.file_docu:write(tostring(node.content[i]))
+        Litua.global.file_docu:write(node:totext())
     end
 end)
 
